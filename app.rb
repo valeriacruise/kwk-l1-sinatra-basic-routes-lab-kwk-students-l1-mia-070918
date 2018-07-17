4f0b5ac0073a5da8652aeed7d1f45be3dfe755bf
valeriacruise <valeriacruise22@gmail.com>
2018-07-17 15:32:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do
+  get '/name' do
+    "My name is Leila!"
+  end
+  
+  get '/hometown' do
+    "My hometown is Miramar!"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Jolene."
   end
 end
